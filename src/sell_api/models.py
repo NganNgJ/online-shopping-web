80fd5b4566d6ca18abd1c3834e65d31b68e71fa6
ngan <thaongan.0912@gmail.com>
2023-12-07 22:35:06 +0700

    Order Item models

--- src/sell_api/models.py
+++ src/sell_api/models.py
@@ -123,3 +123,13 @@ class Order(AbstractEntity, models.Model):
         db_table = 'orders'
         ordering = '-id'
 
+class OrderItem(AbstractEntity, models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_orders')
+    quantity = models.IntegerField()
+    gmv = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        db_table = 'order_items'
+        ordering = '-id'
+
